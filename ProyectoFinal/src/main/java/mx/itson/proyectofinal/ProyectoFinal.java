95ba9060c706708ba456da3b47e582c726c9372f
Evelyn Guzmán <eveli@Evelyn>
2024-10-30 07:56:19 -0700

    Creee el proyecto

---
+++ ProyectoFinal/src/main/java/mx/itson/proyectofinal/ProyectoFinal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.itson.proyectofinal;
+
+/**
+ *
+ * @author eveli
+ */
+public class ProyectoFinal {
+
+    public static void main(String[] args) {
+        
+        
+    }
+}
